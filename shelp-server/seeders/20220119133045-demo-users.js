8df42bf77a5d0cc4cecd15b10ecaadc2068b146e
portis082 <portis082@gmail.com>
2022-01-20 12:40:58 +0900

    ADD sequelize

---
+++ shelp-server/seeders/20220119133045-demo-users.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('users', [{
+      id: 1,
+      username: 'Kim',
+      email: 'example@example.com',
+      password: 'example',
+      image: 'data:image/jpeg;base64,/9j/4AAQSkZJRgSFSDFABAAD/2wC',
+      desc: '자취 1년차',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('users', null, {});
+  }
+};
