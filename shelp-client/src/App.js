fa509f91a00bc29487f19e9ec62d573d96be50df
flowervillagearp <81463737+flowervillagearp@users.noreply.github.com>
2022-01-20 14:32:14 +0900

    [Client] / #3 / Build first deployment  (#17)
    * Add pages (#8)
    
    * Dev (#10)
    
    * Add pages
    
    * Add route feature
    
    * Add testmessage
    
    * [Server] / #5 / add: server configuration (#15)
    
    * FIX gitignore
    
    * FIX package.json
    
    * ADD router
    
    * ADD router content
    
    * ADD sequelize
    
    * ADD routes and controllers
    
    * DELETE router
    
    * [Client] / #3 / Add client configuration (#16)
    
    * Refactor App.js and intro.js
    
    * [Client] mainpage (#12)
    
    * Add pages
    
    * Add route feature
    
    * Add testmessage
    
    * Add mainpage
    
    * Refactor App.js and intro.js (#13)
    
    Co-authored-by: WonjaeJi <jiwonjae@jiwonjaes-MacBook-Pro.local>
    
    * Fix render error in mainpage
    
    * Add buildspec
    
    * Delete .DS_Store
    
    Co-authored-by: WonjaeJi <jiwonjae@jiwonjaes-MacBook-Pro.local>
    Co-authored-by: flowervillagearp <81463737+flowervillagearp@users.noreply.github.com>
    
    Co-authored-by: portis082 <68521666+portis082@users.noreply.github.com>
    Co-authored-by: AJ <86043065+WonjaeJi0801@users.noreply.github.com>
    Co-authored-by: WonjaeJi <jiwonjae@jiwonjaes-MacBook-Pro.local>

--- shelp-client/src/App.js
+++ shelp-client/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { Routes, Route, Link } from "react-router-dom";
+// react-router-dom이 버전 6로 업그레이드되면서 (여기선 v6.2.1 사용중) Switch를 더이상 지원하지 않는다.
+// Switch -> routes로 변경, Component -> element={<Component />}로 변경.
+// https://velog.io/@kcdoggo/Switch-is-not-exported-from-react-router-dom-%EC%97%90%EB%9F%AC
+
+import Intro from "./pages/intro";
+import Main from "./pages/main";
+// import Mypage from "./pages/mypage";
+// import Signup from "./pages/signup";
+
+import { useState } from "react";
+
+export default function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={!isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}
+    //   />
+    // </Routes>
+    <div>{!isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}</div>
   );
 }
-
-export default App;
---
+++ shelp-server/migrations/20220119112101-create-items.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      quantity: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      desc: {
+        type: Sequelize.STRING
+      },
+      group: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      expiration: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('items');
+  }
+};
