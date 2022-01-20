76e82e397420d1a04042a1374aa6bad1434f168c
WonjaeJi <jiwonjae@jiwonjaes-MacBook-Pro.local>
2022-01-20 13:06:00 +0900

    Fix render error in mainpage

--- shelp-client/src/App.js
+++ shelp-client/src/App.js
@@ -1,7 +1,6 @@
-
 import "./App.css";
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 // react-router-dom이 버전 6로 업그레이드되면서 (여기선 v6.2.1 사용중) Switch를 더이상 지원하지 않는다.
 // Switch -> routes로 변경, Component -> element={<Component />}로 변경.
 // https://velog.io/@kcdoggo/Switch-is-not-exported-from-react-router-dom-%EC%97%90%EB%9F%AC
@@ -16,15 +15,13 @@ import { useState } from "react";
 export default function App() {
   const [isLogin, setIsLogin] = useState(false);
 
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={!isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}
-        />
-      </Routes>
-    </BrowserRouter>
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={!isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}
+    //   />
+    // </Routes>
+    <div>{!isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}</div>
   );
 }
