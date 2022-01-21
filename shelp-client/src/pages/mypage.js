b223a88fe9e5eebae1a3f95d45617e460b2ff1b7
flowervillagearp <iyunseong57@gmail.com>
2022-01-21 17:25:05 +0900

    Add features in main and mypage

--- shelp-client/src/pages/mypage.js
+++ shelp-client/src/pages/mypage.js
@@ -1,4 +1,56 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+const axios = require('axios').default;
+
+
 function Mypage () {
+    // const [userInfo, setUserInfo] = useState(null);
+    const [userName, setUserName] = useState(null); // 유저 이름
+    const [userEmail, setUserEmail] = useState(null); // 유저 이메일
+    const [userDesc, setUserDesc] = useState(null); // 유저 설명
+    const [alrm, setAlrm] = useState(null); // 유저 알림 설정
+    const [editBox, setEditBox] = useState(true);
+    const [editBox2, setEditBox2] = useState(true);
+    const [editBox3, setEditBox3] = useState(true);
+
+    const nameModal = (e) => {
+        if(e.target.value) {
+            console.log('noting')
+        }else{
+            console.log('something')
+        }
+        setEditBox(!editBox);
+        // console.log(e.target.value);
+        // axios({
+        //     method: 'put',
+        //     url: '',
+        //     data: {
+        //         "username": 
+        //     }
+        // })
+    } 
+    const emailModal = (e) => {   
+        setEditBox2(!editBox2);
+    }
+    const descModal = (e) => {
+        setEditBox3(!editBox3);
+    }
+
+    const handleChangeName = (e) => {
+        setUserName(e.target.value);
+        console.log(userName)
+    }
+    const handleChangeEmail = (e) => {
+        setUserEmail(e.target.value);
+    }
+    const handleChangeDesc = (e) => {
+        setUserDesc(e.target.value);
+    }
+    const handleRadio = (e) => {    
+        console.log(alrm)
+        setAlrm(e.target.value);
+    }
+
     return (
         <div>
             <section>
@@ -7,26 +59,41 @@ function Mypage () {
                 </div>
                 <div>
                     <ul>
+                        {editBox?
                         <li>
                             pepe
-                            <button>edit</button>
-                        </li>
+                            <button onClick={nameModal}>edit</button>
+                        </li>:
+                        <li>
+                            <input className='user-name' onChange={handleChangeName}/>
+                            <button onClick={nameModal}>save</button>
+                        </li>}
+                        {editBox2?
                         <li>
                             pepepthefrog29@gmail.con
-                            <button>edit</button>
-                        </li>
+                            <button onClick={emailModal}>edit</button>
+                        </li>:
+                        <li>
+                            <input className='user-email' onChange={handleChangeEmail}/>
+                            <button onClick={emailModal}>save</button>
+                        </li>}
+                        {editBox3?
                         <li>
                             feels good man
-                            <button>edit</button>
-                        </li>
+                            <button onClick={descModal}>edit</button>
+                        </li>:
+                        <li>
+                            <input className='user-desc' onChange={handleChangeDesc}/>
+                            <button onClick={descModal}>save</button>
+                        </li>}
                     </ul>
                 </div>
             </section>
             <setting>
                 <div className='option1'>
-                    <input type='radio' name='setalrm' value='1d' />
-                    <input type='radio' name='setalrm' value='3d' />
-                    <input type='radio' name='setalrm' value='5d' />
+                    <button type='radio' name='setalrm' value='1d' onClick={handleRadio} >1d</button>
+                    <button type='radio' name='setalrm' value='3d' onClick={handleRadio} >3d</button>
+                    <button type='radio' name='setalrm' value='5d' onClick={handleRadio} >5d</button>
                 </div>
                 <div>
                     option2
@@ -39,6 +106,7 @@ function Mypage () {
                 <button>save</button>
             </div>
         </div>
+    
     )
 }
 export default Mypage;
