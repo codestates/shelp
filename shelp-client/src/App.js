be3fcd12a0670cdf7ec3c98ef233c302f9f190f7
AJ <jiwonjae@jiwonjaes-MacBook-Pro.local>
2022-01-22 16:53:07 +0900

    Refactor App as change routing hierarchy

--- shelp-client/src/App.js
+++ shelp-client/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Routes, Route, Link, Redirect } from "react-router-dom";
 import Intro from "./pages/intro";
 import Main from "./pages/main";
+import Mypage from "./pages/mypage";
 import axios from "axios";
 import "./App.css";
 import Signup from "./pages/signup";
@@ -9,20 +10,25 @@ import Signup from "./pages/signup";
 // Switch -> routes로 변경, Component -> element={<Component />}로 변경.
 // https://velog.io/@kcdoggo/Switch-is-not-exported-from-react-router-dom-%EC%97%90%EB%9F%AC
 
-// import Mypage from "./pages/mypage";
 // import Signup from "./pages/signup";
 const serverUrl = "https://localhost:4000";
 
 export default function App() {
   const [isLogin, setIsLogin] = useState(false);
-  const [userinfo, setUserinfo] = useState(null);
+  const [userinfo, setUserinfo] = useState({
+    name: "pepe",
+    email: "pepe@gmail.com",
+    desc: "hellp i am pepe",
+  });
+
   const isAuthenticated = () => {
     if (userinfo !== null) {
       setIsLogin(true);
     }
   };
 
-  const handleResponseSuccess = () => {
+  const handleResponseSuccess = (data) => {
+    setUserinfo(data);
     isAuthenticated();
   };
 
@@ -38,13 +44,37 @@ export default function App() {
   }, []);
 
   return (
-    <Routes>
+    <div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            !isLogin ? (
+              <Intro
+                setIsLogin={setIsLogin}
+                handleResponseSuccess={handleResponseSuccess}
+              />
+            ) : (
+              <Main />
+            )
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/mypage"
+          element={<Mypage userinfo={userinfo} setUserinfo={setUserinfo} />}
+        />
+      </Routes>
+    </div>
+  );
+}
+
+{
+  /* <Routes>
       <Route
         path="/"
-        element={!isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}
+        element={isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}
       />
       <Route path="/signup" element={<Signup />} />
-      {/* <div>{!isLogin ? <Intro setIsLogin={setIsLogin} /> : <Main />}</div> */}
-    </Routes>
-  );
+    </Routes> */
 }
